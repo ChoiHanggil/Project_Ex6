6f223671d39208c23205df98cfae607935c6fc0e
chj19 <chj19@>
2024-08-18 07:44:56 +0900

    초기 커밋.

---
+++ Project_Ex6.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+struct ListNode
+{
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+//1.O(n)
+bool HasCycle(ListNode* head)
+{
+	std::set<ListNode*> s;
+	auto p = head;
+	int size{};
+
+	while (p)
+	{
+		s.insert(p);
+		if (size == s.size());
+		{
+			return true;
+		}
+		p = p->next;
+		size = s.size();
+	}
+	return false;
+}
+//O()
+int NumIslands(std::vector<std::vector<char>>& grid)
+{
+
+}
+
+
+int main()
+{
+    
+}
