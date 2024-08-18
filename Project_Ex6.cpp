cf7c944fe61f270dacd73e2e63be961970271a9d
chj1997930@naver.com <chj1997930@naver.com>
2024-08-18 12:13:34 +0900

    완

--- Project_Ex6.cpp
+++ Project_Ex6.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
 #include <set>
 #include <vector>
+#include <queue>
+
 struct ListNode
 {
 	int val;
@@ -29,13 +31,114 @@ bool HasCycle(ListNode* head)
 	}
 	return false;
 }
-//O()
-int NumIslands(std::vector<std::vector<char>>& grid)
+//2.O()
+void LandResearch(std::vector<std::vector<char>> grid, std::vector<std::vector<int>>& temp, int x, int y)
 {
-	
+	static const int dRow[4]{ -1,1,0,0 };
+	static const int dCol[4]{ 0,0,-1,1 };
+
+	std::queue<std::pair<int, int>> q;
+	q.push({ x,y });
+	temp[x][y] = 1;
+
+	while (!q.empty())
+	{
+		x = q.front().first;
+		y = q.front().second;
+		q.pop();
+		for (int i = 0; i < 4; i++)
+		{
+			int dx = x + dRow[i];
+			int dy = y + dCol[i];
+			if (dx < 0 || dx >= grid.size() || dy < 0 || dy >= grid[0].size())
+			{
+				continue;
+			}
+			if (grid[dx][dy] == '1'&& temp[dx][dy] == 0 )
+			{
+				q.push({ dx,dy });
+				temp[dx][dy] = 1;
+			}
+		}
+	}
+
 }
 
-//4.
+int NumIslands(std::vector<std::vector<char>>& grid)
+{
+	int answer{};
+	std::vector<std::vector<int>> temp(grid.size(), std::vector<int>(grid[0].size(), 0));
+	
+	for (int i = 0; i < grid.size(); i++)
+	{
+		for (int j = 0; j < grid[0].size(); j++)
+		{
+			if (grid[i][j] == '1' && temp[i][j] == 0)
+			{
+				LandResearch(grid, temp, i, j);
+				answer++;
+			}
+		}
+	}
+	return answer;
+}
+
+//3.
+int TurnoverCount(std::vector<std::vector<int>> temp, int x, int y)
+{
+	static const int dRow[8]{ -1,-1,0,1,1,1,0,-1 };
+	static const int dCol[8]{ 0,1,1,1,0,-1,-1,-1, };
+
+	int count = 0;
+	int r{}, c{};
+
+	std::priority_queue<int> max;
+	max.push(0);
+
+	for (int i = 0; i < 8; i++)
+	{
+		if (temp[x + dRow[i]][y + dCol[i]] == 2)
+		{
+			r = dRow[i];
+			c = dCol[i];
+			while (temp[x + r][y + c])
+			{
+				r += dRow[i];
+				c += dCol[i];
+				count++;
+				if (temp[x + r][y + c] == 1)
+				{
+					max.push(count);
+					break;
+				}
+			}
+			count = 0;
+		}
+	}
+	return max.top();
+}
+int bestMove(std::vector<std::vector<int>>& board)
+{
+	std::vector<std::vector<int>> temp(board.size()+2, std::vector<int>(board[0].size() + 2, 0));
+	std::priority_queue<int> count;
+	for (int i = 0; i < board.size(); i++)
+	{
+		for (int j = 0; j < board[0].size(); j++)
+		{
+			temp[i + 1][j + 1] = board[i][j];
+		}
+	}
+
+	for (int i = 1; i < 9; i++)
+	{
+		for (int j = 1; j < 9; j++)
+		{
+			count.push(TurnoverCount(temp, i, j));
+		}
+	}
+	return count.top();
+}
+//4.O(m*n)
 int UniquePaths(int m, int n)
 {
 	std::vector<std::vector<int>> v(m+1, std::vector<int>(n+1, 0));
@@ -48,13 +151,47 @@ int UniquePaths(int m, int n)
 	}
 	return v[m][n];
 }
+//5.O(n)
 std::vector<std::vector<int>> MergeIntervals(std::vector<std::vector<int>>& intervals)
 {
-	int max = intervals[intervals.size() - 1][intervalse[0].size() - 1];
+	std::vector<std::vector<int>> answer;
+	answer.push_back(intervals[0]);
 
+	for (auto i = 1;i < intervals.size(); i++)
+	{
+		if (intervals[i][0] < answer.back()[1])
+		{
+			answer.back()[1] = std::max(intervals[i][1], answer.back()[1]);
+		}
+		else
+		{
+			answer.push_back(intervals[i]);
+		}
+	};
+
+	return answer;
 }
 
 int main()
 {
-    
+	//std::vector<std::vector<char>> grid = {
+	//	{'1', '1', '1', '1', '0'},
+	//	{'1', '1', '0', '1', '0'},
+	//	{'1', '1', '0', '0', '0'},
+	//	{'0', '0', '0', '0', '0'}
+	//};
+	//std::cout << NumIslands(grid);
+
+	//std::vector<std::vector<int>> board = {
+	// {0, 0, 0, 0, 0, 0, 0, 0},
+	// {0, 0, 0, 0, 0, 0, 0, 0},
+	// {0, 0, 0, 1, 1, 0, 0, 0},
+	// {0, 0, 0, 1, 0, 0, 0, 0},
+	// {0, 0, 1, 1, 2, 0, 0, 0},
+	// {0, 0, 2, 2, 2, 0, 0, 0},
+	// {0, 0, 0, 0, 2, 1, 0, 0},
+	// {0, 0, 0, 0, 0, 0, 0, 0}
+	//};
+
+	//std::cout << bestMove(board);
 }
